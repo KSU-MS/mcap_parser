1b57d113a1dbbb6bc3d56ce9e0bbd24d654c568e
Rowan Golden <rowancgolden@gmail.com>
2024-07-06 14:14:55 -0400

    Taking a break for calc

--- gui/gooey.py
+++ gui/gooey.py
@@ -1,28 +1,33 @@
 import tkinter as tk
 from tkinter import filedialog
 from tkinterdnd2 import DND_FILES, TkinterDnD
-from utils.file_utils import drop, select_files
+from utils.file_utils import drop, select, upload, handle_files
 
 
-def start_gui():
-
+def start_gui(): # use grid(column,pady) to make prettier
     app = TkinterDnD.Tk()
     app.title("MCAP Parser")
-    # app.iconbitmap -> put scrappy!
     app.geometry("1000x550")
-
-    parser = ConfigParser() # no work
-    parser.read("saved_paths.txt") 
-    grab_dir = parser.get('user_paths', 'grab')
-    upload_dir = parser.get('user_paths', 'upload')
 
     app.drop_target_register(DND_FILES)
     app.dnd_bind("<<Drop>>", drop(app))
+    choosefiles = tk.Button(app, text="Select", command=select)
+    choosefiles.pack(pady=20) 
 
-    select_button = tk.Button(app, text="Select", command=select_files)
-    select_button.pack(pady=20)
+    filedestination = tk.Button(app, text="Destination", command=upload)
+    filedestination.pack(pady=20)
 
-    #  progress_bar = ttk.Progressbar(root, mode="indeterminate")
+    selected = tk.StringVar(value="OMNI") # include saved selection in userinfo
+    omni = tk.Radiobutton(app, text="OMNI", variable=selected, value="OMNI")
+    omni.pack(pady=20)
+    tvn = tk.Radiobutton(app, text="TVN", variable=selected, value="TVN")
+    tvn.pack(pady=20)
+    selectedstyle = selected.get()
+
+    process = tk.Button(app, text="Parse", command=handle_files)
+    process.pack(pady=20)
+
+    # progress_bar = ttk.Progressbar(root, mode="indeterminate")
     # progress_bar.pack(pady=20)
 
     app.mainloop()
--- gui/utils/file_utils.py
+++ gui/utils/file_utils.py
@@ -3,28 +3,38 @@ from parser.parse_man import parse_folder, process_file
 from configparser import ConfigParser
 
 
-def handle_files(folders): # handle them accordingly (in mysterious voice)
-    dest = filedialog.askdirectory(title="Upload Dir")
+def userinfo():
+    parser = ConfigParser()
+    parser.read("saved_paths.txt") 
+
+    def origin():
+        grab_dir = parser.get('user_paths', 'grab')
+        return grab_dir
     
-    for folder in folders: 
-        if 'dir': 
-            parse_folder(folder, dest, "OMNI") 
-        if 'file':
-            process_file()
-        else: 
-            continue 
+    def destination():
+        upload_dir = parser.get('user_paths', 'upload')
+        return upload_dir
+
+
+def select():
+    ddir = userinfo.origin()
+    if (userinfo):
+        ddir = userinfo.o
+
+    files = filedialog.askopenfilenames(title="Select", initialdir=ddir)
+    return files
 
 
 def drop(root, event): # dnd functionality (MEOW!)
     files = root.tk.splitlist(event.data)
-    
-    if files:
-        handle_files(files)
+    return files
 
 
-def select_files():  
-    files = filedialog.askopenfilenames(title="Select")
-    
-    if files:
-        handle_files(files)
+def upload():
+    dest = filedialog.askdirectory(title="Upload Dir", initialdir=userinfo.destination)
+    return dest
 
+
+def handle_files(select, dest, style): 
+    parse_folder(select, dest, style="OMNI")
+    process_file()
