39bbb2d23aa48a5f592dfb777efb2fa2e4239926
ChanceCtrl <StaticElite05@gmail.com>
2024-08-15 11:52:08 -0400

    Cleaning up args

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import sys
-from parser.parse_man import process_file
-from gui.gooey import setupGUI
+import argparse
+from parser.parse_man import parse
+from gui.gooey import gui
 
 
 def main():
-    # parse_folder(sys.argv[1], sys.argv[2], "OMNI")
-    setupGUI(True, process_file) # difficulty accessing parseman from gooey
-    
+    # TODO: Make the sys arg system not fucking aids and hardcodded to shit
+    if sys.argv[1].__contains__("-"):
+        if sys.argv[1] == "--help":
+            print("Basic tool to parse mcaps with a GUI for the casuals")
+            print("-h   --headless run parser with no GUI")
+            # print("-x   --experimental wacky multithreading better for larger files")
+            # print("-n   --no-preserve does not try to preserve file structure")
+            print("")
+            print("headless example:    python -h ./main.py ~/mcaps/ ~/parsed_mcaps/")
+
+        else:
+            if sys.argv[1].__contains__("h" or "headless"):
+                parse(sys.argv[2], sys.argv[3], "OMNI")
+
+    else:
+        gui(parse)
+
 
 if __name__ == "__main__":
     main()
