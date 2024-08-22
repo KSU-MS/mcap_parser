735dbd9029d4369d063125cfbddaea644bb46d3a
Rowan Golden <rowancgolden@gmail.com>
2024-08-22 13:14:58 -0400

    Customtkinter usage (needs work on progress_bar, persistence, DND)

--- gui/gooey.py
+++ gui/gooey.py
@@ -1,32 +1,37 @@
 import tkinter as tk
-from tkinter import ttk
 import customtkinter as ctk
 from tkinterdnd2 import DND_FILES, TkinterDnD
-import gui.utils.file_utils as utilities
+import gui.utils.file_utils as Utilities
 
-persistence = utilities.persistence()
-utils = utilities.front_end()
 
 class Application(ctk.CTk):
     def __init__(self, parse):
         super().__init__()
+        layout = Utilities.LayoutManager(self)
+        persistence = Utilities.Persistence()
+        self.utils = Utilities.InputMethods()
 
-        self.upload_path = tk.StringVar()
-        self.deload_path = tk.StringVar()
-        self.parse_form = tk.StringVar(value='OMNI')
+        self.upload_path = ctk.StringVar(value="Upload dir")
+        self.deload_path = ctk.StringVar(value="Deload dir")
+        self.parse_form = ctk.StringVar(value='OMNI')
 
         ctk.set_appearance_mode("Dark")
-        self.upload_path.set("Upload dir")
-        self.deload_path.set("Deload dir")
-
         self.title("MCAP Parser")
         self.geometry("800x500")
 
-        self.left_frame_init = left_frame(self)
-        self.right_frame_init = right_frame(self)
-        self.middle_frame_init = middle_frame(self, parse)
-        self.progress_bar = ttk.Progressbar(self, mode="indeterminate")
-
+        self.fg_color = '#1f1f1f'
+        self.left_frame_init = LeftFrame(self)
+        self.middle_frame_init = MiddleFrame(self, parse)
+        self.right_frame_init = RightFrame(self)
+        self.progress_bar = ctk.CTkProgressBar(self, mode="indeterminate")
+        
+        layout.configure_grid()
+        layout.place_frames(
+            left_frame = self.left_frame_init,
+            right_frame = self.right_frame_init,
+            middle_frame = self.middle_frame_init,
+            progress_bar = self.progress_bar
+        )
         persistence.load_files(
             left = self.left_frame_init.llistbox, 
             right = self.right_frame_init.rlistbox
@@ -38,61 +43,64 @@ class Application(ctk.CTk):
             )
         )
 
-class left_frame(tk.Frame):
+class LeftFrame(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(parent)
 
         self.grid(row=0, column=0, sticky="nsew")
 
-        self.label_left = tk.Label(self, textvariable=parent.upload_path, width=20, height=2, anchor="w")
+        self.label_left = ctk.CTkLabel(self, textvariable=parent.upload_path, width=20, height=2, anchor="w")
         self.label_left.pack(pady=10, padx=10, anchor="w", fill="x")
 
-        self.llistbox = tk.Listbox(self, selectmode=tk.SINGLE, background="#ffe0d6")
+        self.llistbox = tk.Listbox(self, selectmode=tk.SINGLE, background="#C0C0C0")
         self.llistbox.pack(pady=10, padx=10, anchor="w", fill="x")
         # llistbox.drop_target_register(DND_FILES)
         # llistbox.dnd_bind("<<Drop>>", command=lambda: utils.drop(upload_path, llistbox))
 
-        self.llistbox_but = tk.Button(self, text="Select", command=lambda: utils.select_button())
+        self.llistbox_but = ctk.CTkButton(self, text="Select", command=lambda: parent.utils.select_button())
         self.llistbox_but.pack(pady=10, padx=10)
 
-        self.lbutton = tk.Button(self, text="Open Files", command=lambda: utils.select(parent.upload_path, self.llistbox))
+        self.lbutton = ctk.CTkButton(self, text="Open Files", command=lambda: parent.utils.select(parent.upload_path, self.llistbox))
         self.lbutton.pack(pady=10, padx=10, anchor="w", fill="x")
 
-class right_frame(tk.Frame):
+class RightFrame(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(parent)
 
         self.grid(row=0, column=2, sticky="nsew")
 
-        self.label_right = tk.Label(self, textvariable=parent.deload_path, width=20, height=2, anchor="w")
+        self.label_right = ctk.CTkLabel(self, textvariable=parent.deload_path, width=20, height=2, anchor="w")
         self.label_right.pack(pady=10, padx=10, anchor="w", fill="x")
 
-        self.rlistbox = tk.Listbox(self, selectmode=tk.SINGLE, background="#ffe0d6")
+        self.rlistbox = tk.Listbox(self, selectmode=tk.SINGLE, background="#C0C0C0")
         self.rlistbox.pack(pady=10, padx=10, anchor="w", fill="x")
         # rlistbox.drop_target_register(DND_FILES)
         # rlistbox.dnd_bind("<<Drop>>", utils.drop(deload_path, rlistbox))
 
         # rlistbox.bind("<<ListboxSelect>>", select_down) # add so we can get rid of select button
-        self.rlistbox_but = tk.Button(self, text="Select", command=lambda: utils.select_button())
+        self.rlistbox_but = ctk.CTkButton(self, text="Select", command=lambda: parent.utils.select_button())
         self.rlistbox_but.pack(pady=10, padx=10)
 
-        self.rbutton = tk.Button(self, text="Open Folder", command=lambda: utils.select(parent.deload_path, self.rlistbox))
+        self.rbutton = ctk.CTkButton(self, text="Open Folder", command=lambda: parent.utils.select(parent.deload_path, self.rlistbox))
         self.rbutton.pack(pady=10, padx=10, anchor="w", fill="x")
 
-class middle_frame(tk.Frame):
+class MiddleFrame(ctk.CTkFrame):
     def __init__(self, parent, parse):
         super().__init__(parent)
 
         self.grid(row=0, column=1, sticky="nsew")
 
-        omni = tk.Radiobutton(self, text="OMNI", variable=parent.parse_form, value="OMNI")
+        omni = ctk.CTkRadioButton(self, text="OMNI", variable=parent.parse_form, value="OMNI")
         omni.pack(pady=10, anchor="s", fill="x")
-        tvn = tk.Radiobutton(self, text="TVN", variable=parent.parse_form, value="TVN")
+        tvn = ctk.CTkRadioButton(self, text="TVN", variable=parent.parse_form, value="TVN")
         tvn.pack(pady=10, anchor="s", fill="x")
 
-        process = tk.Button(self, text="Parse", command=lambda: utilities.handle_files(
-                parent.upload_path, parent.deload_path, parent.parse_form, parse
-                )
+        process = ctk.CTkButton(self, text="Parse", command=lambda: Utilities.handle_files(
+                parent.upload_path, 
+                parent.deload_path, 
+                parent.parse_form, 
+                parse
+            )
         )
         process.pack(pady=10, anchor="s", fill="x")
 
--- gui/utils/file_utils.py
+++ gui/utils/file_utils.py
@@ -4,7 +4,26 @@ import json, os
  
 SAVE_FILE = "saved_files.json"
 
-class front_end: # abstract GUI-dependent code
+class LayoutManager:
+    def __init__(self, parent):
+        self.parent = parent
+
+    def configure_grid(self):
+        self.parent.grid_rowconfigure(0, weight=1)
+        self.parent.grid_columnconfigure(0, weight=1)  # Left Frame
+        self.parent.grid_columnconfigure(1, weight=2)  # Middle Frame
+        self.parent.grid_columnconfigure(2, weight=1)  # Right Frame
+
+        self.parent.grid_rowconfigure(1, weight=0) # progress bar row
+
+    def place_frames(self, left_frame, middle_frame, right_frame, progress_bar):
+        left_frame.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
+        middle_frame.grid(row=0, column=1, sticky="nsew", padx=10, pady=10)
+        right_frame.grid(row=0, column=2, sticky="nsew", padx=10, pady=10)
+        
+        progress_bar.grid(row=1, column=0, columnspan=3, sticky="ew", padx=20, pady=10)
+
+class InputMethods: # abstract GUI-dependent code
 
     def drop(self, event, location, box):
         event_list = event.data
@@ -28,7 +47,7 @@ class front_end: # abstract GUI-dependent code
         except tk.TclError:
             tk.messagebox.showerror("No file selected in the left listbox.") # gui dependent
 
-class persistence: # needs error handling
+class Persistence: # needs error handling
 
     def save_files(self, left, right):
         data = {
@@ -60,7 +79,9 @@ def handle_files(upload_path, deload_path, parse_form, parse):
     deload = deload_path.get() + "/"
     style = parse_form.get()
 
-    print(upload, deload, style) # just for debugging
-    if (upload != "Upload dir") or (deload != "Deload dir/"): # change to (!= default)
+    # have "default" rather than string, don't like hardcode here
+    if (upload != "Upload dir") and (deload != "Deload dir/"): 
         parse(upload, deload, style)
         # show_progress()
+    else:
+        tk.messagebox.showerror("No path selected") # gui dependent (also doesn't show string)
